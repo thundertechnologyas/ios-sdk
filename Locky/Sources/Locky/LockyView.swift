40a71d336b99ac566cdea82b0419ac082fa136e3
sherwinstar <slzhouzsl@163.com>
2022-10-28 02:01:55 +0800

    add part view ui

--- Locky/Sources/Locky/LockyView.swift
+++ Locky/Sources/Locky/LockyView.swift
@@ -10,6 +10,25 @@ import UIKit
 import SnapKit
 
 public class LockyView: UIView {
+    private let scrollView = UIScrollView()
+    private let startLabel = UILabel()
+    private let firstHintLabel = UILabel()
+    private let emailTextField = UITextField()
+    private let startButton = UIButton()
+    
+    private let verifyLabel = UILabel()
+    private let verifyHintLabel = UILabel()
+    private let codeTextField = UITextField()
+    private let verifyButton = UIButton()
+    
+    private let tokenHintLabel = UILabel()
+    private let tokenTextField = UITextField()
+    
+    private let tenansLabel = UILabel()
+    private let getMobileButton = UIButton()
+    private let getLocksLabel = UILabel()
+    private let getLocksButton = UIButton()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         createSubviews()
@@ -22,5 +41,18 @@ public class LockyView: UIView {
 
 private extension LockyView {
     func createSubviews() {
+        addSubview(scrollView)
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        scrollView.addSubview(startLabel)
+        startLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        startLabel.text = "Step 1 - Start"
+        startLabel.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(15)
+            make.top.equalToSuperview().offset(15)
+            make.right.equalToSuperview().offset(-15)
+            make.height.equalTo(18)
+        }
     }
 }
