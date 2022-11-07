d11ac32266dd13d834b5b2f7071af143a29e6dca
sherwinstar <slzhouzsl@163.com>
2022-11-07 22:33:15 +0800

    refactor sdk structure.

---
+++ Locky/Sources/Locky/Locky.swift
@@ -0,0 +1,15 @@
+//
+//  Locky.swift
+//  
+//
+//  Created by Shaolin Zhou on 2022/11/7.
+//
+
+import Foundation
+
+public struct Locky {
+    public private(set) var text = "Locky"
+
+    public init() {
+    }
+}
