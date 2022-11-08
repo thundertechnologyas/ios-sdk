6e321f0fe450718b43b16c774e8cc9fc9f14abf8
sherwinstar <slzhouzsl@163.com>
2022-11-08 23:19:09 +0800

    custom the path of source code

--- Package.swift
+++ Package.swift
@@ -19,9 +19,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Locky",
-            dependencies: ["Alamofire"]),
-        .testTarget(
-            name: "LockyTests",
-            dependencies: ["Locky"]),
+            dependencies: ["Alamofire"],
+            path: "Locky"),
     ]
 )
